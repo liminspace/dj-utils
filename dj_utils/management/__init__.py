4a0115a7703fc007d4576304ed4faeecf8240d6f
liminspace <igormeln@gmail.com>
2015-01-01 22:23:58 +0200

    LoggingBaseCommand easy fix.

--- dj_utils/management/__init__.py
+++ dj_utils/management/__init__.py
@@ -4,6 +4,7 @@ import datetime
 import inspect
 import os
 from django.core.management import BaseCommand
+from django.utils.encoding import force_unicode
 from dj_utils import settings as u_settings
 
 
@@ -11,14 +12,19 @@ class LoggingBaseCommand(BaseCommand):
     log_fn = None
     log_dir = u_settings.LOG_DIR
 
-    def log(self, msg, add_time=True, stdout=True, double_br=False):
-        f = open(os.path.join(self.log_dir, self.get_log_fn()), 'a')
-        msg += '\n' * (int(bool(double_br)) + 1)
+    def log(self, msg, add_time=True, stdout=True, double_br=False, ending=None):
+        msg = force_unicode(msg, errors='replace')
+        if double_br:
+            msg += '\n\n'
         if add_time:
             msg = u'[%s] %s' % (datetime.datetime.now().strftime(u'%d.%m.%Y %H:%M:%S'), msg)
         if stdout:
-            self.stdout.write(msg)
-        f.write(msg.encode('utf8'))
+            self.stdout.write(msg, ending=ending)
+        with open(os.path.join(self.log_dir, self.get_log_fn()), 'a') as f:
+            f_ending = '\n' if ending is None else ending
+            if f_ending and not msg.endswith(f_ending):
+                msg += f_ending
+            f.write(msg.encode('utf8'))
 
     def get_log_fn(self):
         if self.log_fn is None:
