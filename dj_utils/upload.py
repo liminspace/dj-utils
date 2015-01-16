842080ea4da7e68137a382a40f6fcba6eddd381d
liminspace <igormeln@gmail.com>
2015-01-16 19:17:41 +0200

    Update functions for upload.

--- dj_utils/upload.py
+++ dj_utils/upload.py
@@ -4,23 +4,51 @@ import os
 import re
 import datetime
 from django.conf import settings
+from django.utils.crypto import get_random_string
 from dj_utils.tools import datetime_to_dtstr, dtstr_to_datetime
 
 
-TMP_PREFIX = '__tmp_'
+TMP_PREFIX = '__tmp__'
 
 
-def generate_filename(ext, label=None, tmp=True):
+def generate_filename(ext=None, label=None):
     """
     Генерує ім'я фалу.
-    Якщо tmp=True, тоді до імені файлу буде додано префікс, який вказуватиме, що файл тимчасовий.
     """
-    return '%s%s%s.%s' % (
-        TMP_PREFIX if tmp else '',
-        datetime_to_dtstr(),
-        '_' + label if label else '',
-        ext
+    if ext and not ext.startswith('.'):
+        ext = '.' + ext
+    if label:
+        label = re.sub(r'[^a-z0-9_\-]', '', label, flags=re.I)[:60]
+    return '{dtstr}_{rand}{label}{ext}'.format(
+        dtstr=datetime_to_dtstr(),
+        rand=get_random_string(4, 'abcdefghijklmnopqrstuvwxyz0123456789'),
+        label='_' + label if label else '',
+        ext=ext or '',
     )
+
+
+def add_tmp_prefix_to_filename(filename):
+    """ Додає префікс тимчасового файлу до імені файлу """
+    return TMP_PREFIX + filename
+
+
+def add_ending_to_filename(filename, ending):
+    """ Додає закінчення до імені файлу """
+    ending = re.sub(r'[^a-z0-9_\-]', '', ending, flags=re.I)[:60]
+    if ending:
+        fn, ext = os.path.splitext(filename)
+        filename = '{fn}_{ending}{ext}'.format(fn=fn, ending=ending, ext=ext)
+    return filename
+
+
+def get_subdir_for_filename(filename):
+    """ Повертає назву підпапки для файлу (dtstr[-2:]) """
+    if filename.startswith(TMP_PREFIX):
+        filename = filename[len(TMP_PREFIX):]
+    m = re.match(r'^([a-z0-9]+?)_.+', filename)
+    if m:
+        return m.group(1)[-2:]
+    return 'other'
 
 
 def url_to_fn(url):
@@ -43,7 +71,7 @@ def remove_file_by_url(url):
 
 
 def move_to_permalink(url):
-    r = re.compile(r'^(.+?)(%s)(.+?)$' % TMP_PREFIX, re.IGNORECASE)
+    r = re.compile(r'^(.+?)(%s)(.+?)$' % TMP_PREFIX, re.I)
     url_m = r.match(url)
     if url_m:
         fn = url_to_fn(url)
@@ -68,7 +96,7 @@ def remove_old_tmp_files(subdirs, max_lifetime=(7 * 24), tmp_prefix=TMP_PREFIX):
     for subdir in subdirs:
         path = os.path.join(settings.MEDIA_ROOT, subdir)
         old_dt = datetime.datetime.utcnow() - datetime.timedelta(hours=max_lifetime)
-        r = re.compile(r"^(%s)([a-z0-9_\-]+?)((?:_.+?)?)(\.[a-z]{3,4})$" % tmp_prefix, re.IGNORECASE)
+        r = re.compile(r"^(%s)([a-z0-9_\-]+?)((?:_.+?)?)(\.[a-z]{3,4})$" % tmp_prefix, re.I)
         for fn in os.listdir(path):
             m = r.match(fn)
             if m:
