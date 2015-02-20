6daada4448eed16ef8e33c8ce3f742bcbe530c18
liminspace <igormeln@gmail.com>
2015-02-21 00:13:00 +0200

    Add http400.

--- dj_utils/views/error.py
+++ dj_utils/views/error.py
@@ -5,6 +5,11 @@ from django.views.decorators.csrf import requires_csrf_token
 
 
 @requires_csrf_token
+def http400(request):
+    return render(request, 'http_error/400.html', {'request_path': request.get_full_path()}, status=400)
+
+
+@requires_csrf_token
 def http403(request):
     return render(request, 'http_error/403.html', {'request_path': request.get_full_path()}, status=403)
 
