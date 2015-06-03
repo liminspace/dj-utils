687bf9f47f0434668fec01c510f3cdbad3bc60d7
liminspace <igormeln@gmail.com>
2015-06-04 00:45:59 +0300

    Update LoggingBaseCommand. Update version to 0.1.71

--- dj_utils/management/__init__.py
+++ dj_utils/management/__init__.py
@@ -10,11 +10,16 @@ from dj_utils import settings as u_settings
 
 class LoggingBaseCommand(BaseCommand):
     log_fn = None
+    log_err_fn = None
     log_dir = u_settings.LOG_DIR
+    log_out_enabled = True
+    log_err_out_enabled = True
 
-    def log(self, msg, add_time=True, out=True, double_br=False, ending=None, std_stream=None):
+    def log(self, msg, add_time=True, out=None, double_br=False, ending=None, std_stream=None, is_error=False):
         if std_stream is None:
             std_stream = self.stdout
+        if out is None:
+            out = self.log_out_enabled
         msg = force_unicode(msg, errors='replace')
         if double_br:
             msg += '\n\n'
@@ -22,16 +27,25 @@ class LoggingBaseCommand(BaseCommand):
             msg = u'[%s] %s' % (datetime.datetime.now().strftime(u'%d.%m.%Y %H:%M:%S'), msg)
         if out:
             std_stream.write(msg, ending=ending)
-        with open(os.path.join(self.log_dir, self.get_log_fn()).replace('\\', '/'), 'a') as f:
+        with open(os.path.join(self.log_dir, self.get_log_fn(is_error=is_error)).replace('\\', '/'), 'a') as f:
             f_ending = '\n' if ending is None else ending
             if f_ending and not msg.endswith(f_ending):
                 msg += f_ending
             f.write(msg.encode('utf8'))
 
-    def log_err(self, msg, add_time=True, out=True, double_br=False, ending=None):
-        self.log(msg, add_time=add_time, out=out, double_br=double_br, ending=ending, std_stream=self.stderr)
+    def log_err(self, msg, add_time=True, out=None, double_br=False, ending=None):
+        if out is None:
+            out = self.log_err_out_enabled
+        self.log(msg, add_time=add_time, out=out, double_br=double_br, ending=ending, std_stream=self.stderr,
+                 is_error=True)
 
-    def get_log_fn(self):
-        if self.log_fn is None:
-            return '%s.log' % os.path.splitext(os.path.basename(inspect.getfile(self.__class__)))[0]
-        return self.log_fn
+    def get_log_fn(self, is_error=False):
+        get_cmd_name = lambda: os.path.splitext(os.path.basename(inspect.getfile(self.__class__)))[0]
+        if is_error:
+            if self.log_err_fn is None:
+                return '{}.error.log'.format(get_cmd_name())
+            return self.log_err_fn
+        else:
+            if self.log_fn is None:
+                return '{}.log'.format(get_cmd_name())
+            return self.log_fn
