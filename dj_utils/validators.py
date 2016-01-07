9caf205d54f6d7b76f54fa98aeaf835595063018
liminspace <igormeln@gmail.com>
2016-01-07 22:50:30 +0200

    Remove moved tools. Update to version 0.3.4

--- dj_utils/validators.py
+++ dj_utils/validators.py
@@ -1,19 +1,6 @@
 # coding=utf-8
-from django.core.exceptions import ValidationError
 from django.core.validators import RegexValidator
-from django.utils.translation import ugettext_lazy, ugettext as _
-from . import settings as u_settings
-
-
-def validate_email_domain(email):
-    """ Валідація email по чорному списку доменів. """
-    try:
-        domain = email.split('@', 1)[1]
-    except IndexError:
-        pass
-    else:
-        if domain.lower() in u_settings.DJU_EMAIL_DOMAIN_BLACK_LIST:
-            raise ValidationError(_(u'Email with domain "%s" is disallowed.') % domain)
+from django.utils.translation import ugettext_lazy
 
 
 validate_phone_number12 = RegexValidator(  # Валідація номеру телефону в форматі +XX XXX XXXXXXX.
